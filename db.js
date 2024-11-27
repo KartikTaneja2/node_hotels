d1a18bf46ac1148a617257ae9b2b5c2e0ece73d3
Kartik Taneja <kartiktaneja@Kartiks-MacBook-Air.local>
2024-11-28 03:41:55 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose'); 
-
+require('dotenv').config();
 // define the Mongodb connection URL //
 
-const mongoURL='mongodb://localhost:27017/hotels' // Replace 'mydatabase' with your database name //
-
+// const mongoURL =process.env.MONGODB_URL_LOCAL  // Replace 'mydatabase' with your database name //
+const mongoURL=process.env.MONGODB_URL;  
 // Set up MongodB connection //
 
 mongoose.connect(mongoURL);
--- server.js
+++ server.js
@@ -97,9 +97,12 @@
 const express=require('express');
 const app=express();
 const db=require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+const PORT=process.env.PORT || 3000;
 
 app.get('/',function(req,res){
     res.send('hello world')
@@ -113,7 +116,8 @@ const menuItemRoutes=require('./routes/menuItemRoutes');
 app.use('/person' , personRoutes);
 app.use('/menu',menuItemRoutes);
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log('listening on port 3000');
 })
 
+ 
